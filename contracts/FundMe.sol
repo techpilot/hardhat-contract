a9523a4007c51ea35a26b0e66102c154318d119a
Stephen Ngwu <ngwustephen99@gmail.com>
2022-08-27 12:00:11 +0100

    updates on smart contracts

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.9;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
+
+/** @title A contract for crowd funding
+ *  @author Stephen
+ *  @notice This contract is to demo a simple fundme contract
+ *  @dev This implements price feeds as our library  
+ */ 
 
 contract FundMe {
     using PriceConverter for uint256;
@@ -13,29 +19,40 @@ contract FundMe {
     address[] public funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+
+    AggregatorV3Interface public priceFeed;
+
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
     
-    constructor() {
+    constructor(address priceFeedAddress) {
         i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value, priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
     
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
-    }
-    
-    modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
     }
     
     function withdraw() payable onlyOwner public {
@@ -53,26 +70,6 @@ contract FundMe {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
-
-    fallback() external payable {
-        fund();
-    }
-
-    receive() external payable {
-        fund();
-    }
-
 }
 
 // Concepts we didn't cover yet (will cover in later sections)
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,22 +4,27 @@ require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  // solidity: "0.8.9",
+  solidity: {
+    compilers: [{ version: "0.8.9" }, { version: "0.6.6" }],
+  },
   defaultNetwork: "hardhat",
   networks: {
     goerli: {
       url: process.env.GOERLI_RPC_URL || "https://eth-goerli",
       accounts: [process.env.PRIVATE_KEY || "0xkey"],
       chainId: 5,
+      blockConfirmations: 6,
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
     },
   },
-  solidity: "0.8.9",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "key",
   },
@@ -29,5 +34,13 @@ module.exports = {
     noColors: true,
     currency: "USD",
     coinmarketcap: process.env.COINMARKETCAP_API_KEY || "key",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
   },
 };
